4695f33f587d2608a1faddfb6242b37d0ed2a773
sz-games <116248710+sz-games@users.noreply.github.com>
2025-01-11 15:26:58 -0800

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -2,7 +2,7 @@
 self.__uv$config = {
     prefix: '/uv/service/',
 
-    bare: 'https://bare.benrogo.net',
+    bare: 'https://bare.benrogo.net/',
     
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
